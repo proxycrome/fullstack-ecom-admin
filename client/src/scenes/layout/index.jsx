f2ba17102c89e5b3fe5f3dda7217d325c926a810
proxycrome <e.v.ezeonwuka@gmail.com>
2025-05-01 19:41:26 +0100

    init commit

---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "state/api";
+
+const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
+
+  return (
+    <Box display={isNonMobile ? "flex" : "Block"} width="100%" height="100%">
+      <Sidebar
+        user={data || {}} // Provide a default value in case data is undefined
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          user={data || {}}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
---
+++ server/controllers/client.js
@@ -0,0 +1,95 @@
+import getCountryISO3 from "country-iso-2-to-3";
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    const productsWithStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({ productId: product._id });
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
+    res.status(200).json(productsWithStats);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getTransactions = async (req, res) => {
+  try {
+    // sort should look like this {"field": "userId", "sort": "desc"}
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+    // formatted sort should look like this { userId: -1 }
+    const generateSort = () => {
+      const parsedSort = JSON.parse(sort);
+      const sortFormatted = {
+        [parsedSort.field]: parsedSort.sort === "asc" ? 1 : -1,
+      };
+      return sortFormatted;
+    };
+
+    const formattedSort = Boolean(sort) ? generateSort() : {};
+
+    const transactions = await Transaction.find({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    })
+      .sort(formattedSort)
+      .skip(page * pageSize)
+      .limit(pageSize);
+
+    const total = await Transaction.countDocuments({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    });
+    res.status(200).json({ transactions, total });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryISO3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
